0e8e59cd3719a56e174e91b709743b9ab66773bb
suin <btj1011@naver.com>
2024-08-30 21:06:06 +0900

    Initial commit

---
+++ IBM-Project-frontend Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IBM-Project-frontend Watch App
+//
+//  Created by Suin Kim on 8/30/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IBM-Project-frontend Watch App/IBM_Project_frontendApp.swift
@@ -0,0 +1,17 @@
+//
+//  IBM_Project_frontendApp.swift
+//  IBM-Project-frontend Watch App
+//
+//  Created by Suin Kim on 8/30/24.
+//
+
+import SwiftUI
+
+@main
+struct IBM_Project_frontend_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
